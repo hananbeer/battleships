251e3bd0922361e5c5dbfab94c9e25a8914f1518
Hanan Beer <email@hanan.beer>
2022-09-07 21:55:39 +0300

    some progress

--- README.md
+++ README.md
@@ -1,13 +1,98 @@
-# Sample Hardhat Project
+# **PROVABLY FAIR PLAYGROUND**
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+Welcome. This repo is a playground for on-chain and off-chain fair gaming concepts!
 
-Try running some of the following tasks:
+Alternative names:
 
-```shell
-npx hardhat help
-npx hardhat test
-GAS_REPORT=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.js
-```
+- Provably Fair Games, **PFG**
+- Cryptographically Secure Games, **CSG**
+- Cryptographically Fair Games, **CFG**
+  - that's my favorite (but also means many other things)
+- On-n-Off-Chain Games, O&OcG or O&OG, OOG, **On-n-Off Games**, On&Off Games
+  - also really like this one
+- Optimistically Proven Games, **OPG** or **OP** Games
+  - that's really catchy too
+
+
+# Provably Fair Battleships
+
+Let's implement the classic game *Battleships*.
+
+## modifications
+
+some modifications are either necessary or desired to create on-chain battleships.
+
+- board size 16x16 (so 256 cells can be represented in uint8)
+- only one ship of each size
+- additional ship of size 6
+- additional non-ship item of size 1x1
+  - this item gives opponent special powers
+  - game will still end even if item was not discovered
+  - could have additional 2 items: one with good effects and another with bad effects (e.g. shield protects = good vs. fuel explodes = bad)
+
+
+## IDEA #1: on-chain flow
+
+  1. open room -> wait for opponent
+  2. join room -> can both(?) start game
+    (could do syn-synack-ack flow)
+    (open = syn, join = synack, start = ack)
+    (open with room id, join room with merkle, start with merkle)
+  3. merkle root of (board + random salt) (where board is 0/1 if ship/noship)
+  each play (or both play at the same time? half duplex vs full duplex)
+    play with missile coordinate
+    (again could be syn-synack?)
+  or could also play off-chain with "blockchain"
+    each play is signed and result is shown on-chain in the end
+  4. either winner sends result or fault proof
+
+
+### IDEA #2: simplified on-chain flow:
+
+1. both players choose board and agree on opponent (0x0 for any)
+2. every 5 or 10 seconds players may choose a location to drop bombs (or randomally if not)
+3. 
+
+### IDEA #3: simplified off-chain flow:
+
+1. both players choose board
+2. every 5 or 10 seconds players may choose a location to drop bombs (or randomally if not)
+3. 
+
+## verification:
+
+- time must move forward
+- plays were not dropped
+- ships must be well-formed (all ships have proper sizes, non-adjacent?)
+  - no missing ships, no extra ships
+  - proofs correspond to committed root (can be done off-chain)
+- cheating can be proved per transition, no need to prove entire game
+- players must call out when ship destroyed
+  - prove fraud if there's an acknowledged state where ship is sunk but not called out.
+
+## communication channel:
+
+- must be agreed beforehand
+- cannot be censored, must be stable
+- no dos, no frontrunning
+
+## communication:
+
+- player 1 sends signed move hash
+- player 2 acknowledges move hash
+
+next message = [syn player 1 move hash #3] [ack player 2 move hash #2] [player 1 move #1]
+64 bytes sync header
+hash can be a rolling hash
+
+or maybe:
+[opcode byte] [data...]
+
+    opcodes: (as 8-bits mask)
+      00 - stop, no-op
+      01 - syn
+      02 - ack
+      03 - synack
+      04 - include move
+      ..
+      ff - all actions
--- contracts/Battleships.sol
+++ contracts/Battleships.sol
@@ -20,67 +20,159 @@ contract Battleships {
     }
 
     struct Player {
+        uint256 game_id;
         //address player;
         uint256 board_merkle_root;
         //address opponent;
-        uint256 last_move; // block.timestamp
+        uint256 last_move; // block.number
         uint8[] missiles;
-        bool is_playing;
+        //bool is_playing;
     }
 
+    enum GameState {
+        None,
+        Open,
+        Joined,
+        Started,
+        Ended,
+        Slashed,
+        Faulted
+    }
+
+    struct Game {
+        uint256 id;
+        address player1;
+        address player2; // thought experiment: can play larger games!
+        GameState state;
+        uint256 start_block; // block.number
+        //uint256 start_time; // block.timestamp
+    }
+
+    Game[] public games;
     mapping (address => Player) public players;
 
-    function hash(uint256[100] calldata board) public {
-        /*
-               root
-            c1      c2
-          c3  c4  c5  c6
-          NOTE: since there are onzly 100 nodes it can be fuzzed easily...  :\ even with salt
-          need to randomize each value, not just salt
-          on missle need to supply proof (lsb controls the bool of the board? or maybe mod X for X options)
-        */
+    // about 30-45 seconds window on arbitrum
+    // used to make sure no player is AFK while another tries to join
+    uint256 immutable MAX_BLOCKS_HIGH_AND_DRY = 100;
+
+    function _game_id() internal view returns (uint256) {
+        return players[msg.sender].game_id;
     }
 
-    // maybe not needed?
-    function Open(uint256 room) public {
+    function _game() internal view returns (Game storage) {
+        // TODO: bound checks
+        return games[_game_id()];
     }
 
-    // maybe not needed?
-    function Join(uint256 room) public {
-        
+    function _player() internal view returns (Player storage) {
+        return players[msg.sender];
     }
 
-    // start a fully joined game
-    function Start(uint256 room, uint256 board_merkle_root) public returns (uint256 game_id) {
+    function _opponent() internal view returns (Player storage) {
+        Game storage game = _game();
+        if (game.player1 == msg.sender)
+            return players[game.player2];
+        else if (game.player2 == msg.sender)
+            return players[game.player1];
+        else
+            revert("no opponent");
+    }
+
+    function _init_player(uint256 board_merkle_root) internal {
         uint8[] memory arr;
-        players[msg.sender] = Player(
-            board_merkle_root,
-            block.timestamp,
-            arr,
-            true
-        );
-
-        return block.number;
+        players[msg.sender] = Player({
+            game_id: _game_id(),
+            board_merkle_root: board_merkle_root,
+            last_move: 0,
+            missiles: arr
+        });
     }
-    
-    // must be turn
-    function Play(uint256 game_id, uint8 coord) public {
+
+    // called by player 1
+    function Open(uint256 board_merkle_root) public {
+        uint256 id = games.length;
+        games.push(Game(
+            id,
+            msg.sender,
+            address(0x0),
+            GameState.Open,
+            0
+        ));
+
+        _init_player(board_merkle_root);
+    }
+
+    // called by player 2
+    function Join(uint256 game_id, uint256 board_merkle_root) public {
+        Game storage game = games[game_id];
+        require(game.player1 != address(0x0), "room not open");
+        require(game.player2 != address(0x0), "room full");
+        game.player2 = msg.sender;
+        game.state = GameState.Joined;
+        _init_player(board_merkle_root);
+    }
+
+    // for ease of use Open & Join accept merkle roots which may represent
+    // a random board state. while game hasn't started, allow players to
+    // remix their board to their liking.
+    function Shuffle(uint256 board_merkle_root) public {
+        require(_game().state < GameState.Started, "too late to shuffle");
+        _player().board_merkle_root = board_merkle_root;
+    }
+
+    function Start() public {
+        require(_game().state < GameState.Started, "too late to shuffle");
+        _player().last_move = block.number;
+        uint256 opponent_last_move = _opponent().last_move;
+        if (opponent_last_move == 0)
+            return;
+        
+        // technically not really needed since the last player to call Start()
+        // will have their .last_move be greater
+        // and in Play() the lower .last_move player has the right to move next
+        if (opponent_last_move - block.number < MAX_BLOCKS_HIGH_AND_DRY) {
+            Game storage game = _game();
+            game.state = GameState.Started;
+            game.start_block = block.number;
+            //emit GameStarted(game_id(), block.number);
+
+            // both players may now call Play()
+        }
+    }
+
+    // must be player's turn
+    function Play(uint8 coord) public {
+        require(_game().state == GameState.Started, "no active game");
+        uint256 player_last_move = _player().last_move;
+        uint256 opponent_last_move = _opponent().last_move;
+        require(player_last_move < opponent_last_move, "not your turn");
+        require(opponent_last_move - player_last_move < MAX_BLOCKS_HIGH_AND_DRY, "stale game");
+
         Player storage player = players[msg.sender];
         player.missiles.push(coord);
         // - must be turn
+
+        _player().last_move = block.timestamp;
     }
 
     //  - must reveal board on-chain within 30 seconds or slash able
-    function End(uint256 game_id, uint8[] calldata ships) public {
+    function End(uint8[] calldata ships) public {
 
     }
     
     function Fault(bytes calldata proof) public {
     }
 
-    function Slash(bytes calldata proof) public {
-        // either too much time without End() called or merkle root don’t match?
-        // - could just make End check merkle proof..
+    function Slash() public {
+        uint256 player_last_move = _player().last_move;
+        uint256 opponent_last_move = _opponent().last_move;
+        require(player_last_move < opponent_last_move, "not your turn");
+        require(opponent_last_move - player_last_move > MAX_BLOCKS_HIGH_AND_DRY, "not slashable");
+
+        // TODO: slash here
+
+        _game().state = GameState.Slashed;
+        //emit GameSlashed();
     }
 
     /*
@@ -96,4 +188,15 @@ contract Battleships {
         uint8 to; // 0-99 ^
     */
 
+
+    function hash(uint256[100] calldata board) public {
+        /*
+               root
+            c1      c2
+          c3  c4  c5  c6
+          NOTE: since there are onzly 100 nodes it can be fuzzed easily...  :\ even with salt
+          need to randomize each value, not just salt
+          on missle need to supply proof (lsb controls the bool of the board? or maybe mod X for X options)
+        */
+    }
 }
